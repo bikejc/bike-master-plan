726baefad635def73d6dbc70c7e4dd3dd3fc2ec7
Ryan Williams <ryan@runsascoded.com>
2022-11-19 21:42:40 -0500

    fix make-slides

--- make-slides.sh
+++ make-slides.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-(for i in `seq 1 168`; do d="$(printf "%03d" "$i")"; echo -n "<a id=\"pg-${i}\"></a>"; echo '![]'"(./imgs/jc%20bike%20master%20plan-${d}.png)"; done) > slides.md
+(for i in `seq 1 168`; do d="$(printf "%03d" "$i")"; echo -n "<a id=\"pg-${i}\"></a>"; echo '![]'"(./imgs/jc%20bike%20master%20plan.pdf-${d}.png)"; done) > slides.md
